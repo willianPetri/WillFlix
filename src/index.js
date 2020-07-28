95dae543a27aaf6a3471e4488009b07fc4bb19e4
Willian Petri <wopetri11@gmail.com>
2020-07-28 15:41:14 -0300

    Updated Carousel and Add folder pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/Cadastro/Video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route component={Pagina404} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
